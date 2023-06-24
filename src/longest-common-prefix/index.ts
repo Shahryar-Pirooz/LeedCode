d2baf3fbc1a828371e5c259adc06e8d633f22023
DevEss <dev.shahryar@gmail.com>
2023-06-25 00:34:36 +0330

    Longest Common Prefix

--- src/longest-common-prefix/index.ts
+++ src/longest-common-prefix/index.ts
@@ -1,3 +1,26 @@
 function longestCommonPrefix(strs: string[]): string {
-	return ''
+	if (!strs.length) return ''
+
+	const firstStr = strs[0]
+	let prefix = ''
+
+	for (let i = 0; i < firstStr.length; i++) {
+		const char = firstStr.charAt(i)
+
+		for (let j = 1; j < strs.length; j++) {
+			const currStr = strs[j]
+
+			if (i >= currStr.length || currStr.charAt(i) !== char) {
+				return prefix
+			}
+		}
+
+		prefix += char
+	}
+
+	return prefix
 }
+
+const strs: string[] = ['flower', 'flow', 'flight']
+
+console.log(longestCommonPrefix(strs))

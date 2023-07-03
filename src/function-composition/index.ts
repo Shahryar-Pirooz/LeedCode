ec3ff78c836a24bbbb3fb096abb59fc5aee63796
DevEss <dev.shahryar@gmail.com>
2023-07-03 22:36:44 +0330

    function-composition

--- src/function-composition/index.ts
+++ src/function-composition/index.ts
@@ -1,10 +1,11 @@
-type F = (x: number) => number;
+type F = (x: number) => number
 
 function compose(functions: F[]): F {
-	return (x) => {
-        return x
-    }
-};
+	return functions.reduce(
+		(preFN, currFN) => (x) => preFN(currFN(x)),
+		(x) => x
+	)
+}
 
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
